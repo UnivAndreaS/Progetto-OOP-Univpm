1942a3b2db03f2b054d4dec50032eb0a62845756
UnivAndreaS <95362575+UnivAndreaS@users.noreply.github.com>
2021-12-15 18:36:08 +0100

    Add files via upload

---
+++ Progetto-OOP-Univpm/openweather/src/main/java/com/univpm/openweather/controller/weatherController.java
@@ -0,0 +1,14 @@
+package com.univpm.openweather.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller 
+public class weatherController {
+	
+	@RequestMapping(value = "/getForecast")
+	
+
+}
+
